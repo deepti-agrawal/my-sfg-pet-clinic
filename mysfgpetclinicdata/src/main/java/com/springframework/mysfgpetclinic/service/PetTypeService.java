b637dfeee3db6477ae8217af981aeac26495d405
Deepti <Deepti.sms@gmail.com>
2019-09-11 15:09:18 -0500

    implement the pet type service. closes #14

---
+++ mysfgpetclinicdata/src/main/java/com/springframework/mysfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springframework.mysfgpetclinic.service;
+
+import com.springframework.mysfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ mysfgpetclinicdata/src/main/java/com/springframework/mysfgpetclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.springframework.mysfgpetclinic.service.map;
+
+import com.springframework.mysfgpetclinic.model.PetType;
+import com.springframework.mysfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findall();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
