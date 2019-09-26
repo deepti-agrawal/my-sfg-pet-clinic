19d4e8b8c6f67ce3565e84a2aa1fcb18442ce148
Deepti <Deepti.sms@gmail.com>
2019-09-26 16:38:46 -0500

    Added Project Lombok to the project.

--- mysfgpetclinicdata/src/main/java/com/springframework/mysfgpetclinic/model/Person.java
+++ mysfgpetclinicdata/src/main/java/com/springframework/mysfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package com.springframework.mysfgpetclinic.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
 
@@ -11,19 +18,9 @@ public class Person extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
+    public Person(Long Id, String firstName, String lastName) {
+        super(Id);
         this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 }
